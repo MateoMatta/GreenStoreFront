76858d91628615e22c8f3731247c70f363301e6f
JuanFerGiraldo <juan.giraldo@rappi.com>
2020-12-01 16:58:20 -0500

    Set up

---
+++ src/components/Home.js
@@ -0,0 +1,15 @@
+import React, { Fragment } from 'react';
+import Hero from './Hero';
+import HomeContent from './HomeContent';
+
+export default function Home() {
+  return (
+    <Fragment>
+      <Hero />
+      <div className="box cta">
+        
+      </div>
+      <HomeContent />
+    </Fragment>
+  )
+}
---
+++ src/components/Navbar.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react'
+
+export default class Navbar extends Component {
+  render() {
+    return (
+      <nav className="navbar" role="navigation" aria-label="main navigation">
+        <div className="navbar-brand">
+          <a className="navbar-item" href="/" style={{color: "#00d1b2", fontWeight: "bold", fontSize: 30, paddingBottom: "7%"}}>
+            <p>Green Store</p>
+          </a>
+        </div>
+
+        <div id="navbarBasicExample" className="navbar-menu">
+          <div className="navbar-start">
+            <a href="/" className="navbar-item">
+              Home
+            </a>
+            <a href="/products" className="navbar-item">
+              Products
+            </a>
+            <a href="/admin" className="navbar-item">
+              Admin
+            </a>
+          </div>
+
+          <div className="navbar-end">
+            <div className="navbar-item">
+              <div className="buttons">
+                <a href="/register" className="button is-primary">
+                  <strong>Register</strong>
+                </a>
+                <a href="/login" className="button is-light">
+                  Log in
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </nav>
+    )
+  }
+}
